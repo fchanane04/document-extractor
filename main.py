9c85a0966c99ba12e442b5acd4022a9b672b0ad3
fchanane04 <chndesign16@gmail.com>
2026-05-25 20:08:22 +0100

    feat: started linking extractors

--- main.py
+++ main.py
@@ -2,4 +2,22 @@
 Command: python3 main.py --file <file.ext> --provider <llm>
 '''
 
+import argparse
+from pathlib import Path
 
+def detect_file_ext(file_path: str) -> str:
+    suffix = Path(file_path).suffix.lower()
+    if not suffix:
+        print("file has no extention, not supported")
+    else:
+        print(f"extention is {suffix}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Document Extractor")
+    parser.add_argument("--file", required=True, help="Path to the file")
+    args = parser.parse_args()
+
+    print(detect_file_ext(args.file))
+
+if __name__ == "__main__":
+    main()
